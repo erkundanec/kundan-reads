0143f59c339fb1cb05a9ef04b0a39c97f8b36149
erkundanec <erkundanec@gmail.com>
2024-02-21 10:21:27 +0530

    update

--- source/conf.py
+++ source/conf.py
@@ -185,4 +185,13 @@ copybutton_prompt_is_regexp = True
 
 html_show_sourcelink = False
 
+# These folders are copied to the documentation's HTML output
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
+
 
